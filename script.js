82fb3fffe71eaef88540a23a4f3bda45edb13801
Regie Marzan <lonebeginner@gmail.com>
2023-04-08 22:06:46 +0800

    add regiepage & replaceState & back2.js for history

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ window.alert('this is the first window.alert ' + history.length + ' value for th
 alert('this is the second alert box ' + history.length + ' window object is implied!');
 
 // history.forward();
+
+// history.replaceState('regie is here', null, 'replaceState.html');
+
+// console.log(history.state);
+
+history.pushState(null,null,'regiepage.html');
+
+// window.history.back();
