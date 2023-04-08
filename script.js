2dee474589aba8da8009236ccd4c44bfe3872441
Regie Marzan <lonebeginner@gmail.com>
2023-04-08 20:56:30 +0800

    add window.alert & alert, back.html & back.js

---
+++ script.js
@@ -0,0 +1,3 @@
+window.alert('this is the first window.alert ' + history.length + ' value for the history.length!');
+
+alert('this is the second alert box ' + history.length + ' window object is implied!');
